9e651f6b7bdffee7c53ca95e55344f3a2ff5ec55
Tania Aglae Ramirez del Real <49289573+taniaglae@users.noreply.github.com>
2025-01-13 16:41:06 -0600

    Update module_VCRScore.py

--- module_VCRScore.py
+++ module_VCRScore.py
@@ -18,27 +18,27 @@ class module_VCRScore:
             warnings.simplefilter("ignore")
             fxn()
 
-    def VCRScore (self,ref,caption,file_image):
+    def VCRScore (self,ref,caption,img):
         model_yolo = yolo.YOLO("YOLO-model.h5")
-        _, yolo_classes, _ = model_yolo.detect(file_image)
+        _, yolo_classes, _ = model_yolo.detect(img)
         
         model_det =detr.DETR()
-        detr_classes = model_det.detect(file_image)
+        detr_classes = model_det.detect(img)
         
         ref_lista = ' '.join(ref)
         pr = PrRe.PrRe(ref_lista,caption,detr_classes,yolo_classes)
         recall_own,precision_own=pr.calcula()
         
         model_vitl =vitl.vitl()
-        score_vitl = model_vitl.score_vitl(caption,file_image)
+        score_vitl = model_vitl.score_vitl(caption,img)
         
         model_clipsc = clipsc.clipsc()
-        sc,score_clipsc = model_clipsc.scoreclip(caption,file_image,ref)
+        sc,score_clipsc = model_clipsc.scoreclip(caption,img,ref)
         
         model_VCRscore = pickle.load(open('model_GBR_VCR.sav', 'rb'))
         #'recall_own','distancia','clip_ref_score'
         values = np.array([recall_own[0],score_vitl,score_clipsc])
         values = values.reshape(1, -1)
         value_VCRScore = model_VCRscore.predict(values)
-        return value_VCRScore[0],sc,score_clipsc,score_vitl,recall_own,precision_own
+        return value_VCRScore[0],sc,score_clipsc,score_vitl
         
